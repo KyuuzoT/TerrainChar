49b6f0c7096e92be6276d12412e036e11d1db398
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2021-01-26 12:56:53 +0300

    Skybox rotation done

---
+++ Assets/Scripts/SkyboxRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotation : MonoBehaviour
+{
+    [SerializeField][Range(0.01f,8.0f)] private float rotationSpeed = 0.4f;
+    private float rotation = 0.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        rotation += Time.deltaTime * rotationSpeed;
+        if(rotation >= 360.0f)
+        {
+            rotation = 0.0f;
+        }
+        RenderSettings.skybox.SetFloat("_Rotation", rotation);
+    }
+}
